534b104eab8db9fcd24e7cc1497c49d083789016
MagdalenaKuch <57178283+MagdalenaKuch@users.noreply.github.com>
2022-08-12 15:45:15 +0200

    Update app.component.ts
    remove unnecessary skretch components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,11 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map, NavigationControl } from 'maplibre-gl';
+import {  Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('map')
-  private mapContainer!: ElementRef<HTMLElement>;
 
-  ngAfterViewInit() {
-    const myAPIKey = 'YOUR_MAPTILER_API_KEY_HERE';
-    const mapStyle = 'https://api.maptiler.com/maps/streets/style.json';
-
-    const initialState = {
-      lng: 11,
-      lat: 49,
-      zoom: 9,
-    };
-
-    const map = new Map({
-      container: this.mapContainer.nativeElement,
-      style: `${mapStyle}?key=${myAPIKey}`,
-      center: [initialState.lng, initialState.lat],
-      zoom: initialState.zoom,
-    });
-
-    map.addControl(new NavigationControl({}), 'top-left');
+  export class AppComponent {
+    title = 'my-ng-app';
   }
-}
