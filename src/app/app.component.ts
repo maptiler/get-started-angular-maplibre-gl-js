06f71b256aaac2a59461498919bd32dd8b6bf766
MagdalenaKuch <57178283+MagdalenaKuch@users.noreply.github.com>
2022-08-12 15:49:18 +0200

    Update src/app/app.component.ts
    Co-authored-by: bolo <bolosig@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-  export class AppComponent {
-    title = 'my-ng-app';
+export class AppComponent {
+  title = 'my-ng-app';
   }
